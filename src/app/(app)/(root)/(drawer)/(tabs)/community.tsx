c2f1cdd1c851114bf4f1c7c5173a53ee70d89e99
Dante Arias <danteariastarifa@gmail.com>
2023-12-11 01:06:02 -0400

    update community

--- src/app/(app)/(root)/(drawer)/(tabs)/community.tsx
+++ src/app/(app)/(root)/(drawer)/(tabs)/community.tsx
@@ -1,44 +1,41 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
-import DefaultLayout from '@/layout/DefaultLayout'
-import { FontAwesome } from '@expo/vector-icons'
-import { ScrollView } from 'react-native-gesture-handler'
-import { Link } from 'expo-router'
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
+import DefaultLayout from "@/layout/DefaultLayout";
+import { FontAwesome } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
+import { Link } from "expo-router";
 
 type Menu = {
   icon: any;
   title: string;
   to: any;
-}
+};
 const menu: Menu[] = [
   {
-    icon: <FontAwesome name='deafness' size={50} color="#FFF" />,
+    icon: <FontAwesome name="deafness" size={50} color="#FFF" />,
     title: "INCIDENTES",
-    to: "/incidents"
+    to: "/incidents",
   },
   {
-    icon: <FontAwesome name='diamond' size={50} color="#FFF" />,
+    icon: <FontAwesome name="diamond" size={50} color="#FFF" />,
     title: "ALGO MAS",
-    to: "/algomas"
+    to: "/algomas",
   },
   {
-    icon: <FontAwesome name='ticket' size={50} color="#FFF" />,
+    icon: <FontAwesome name="ticket" size={50} color="#FFF" />,
     title: "OTRO MAS",
-    to: "/otromas"
+    to: "/otromas",
   },
-
-
-]
+];
 const Community = () => {
   return (
     <DefaultLayout withSafeArea={false}>
-
       <FlatList
         data={null}
-        contentContainerClassName='p-4'
+        contentContainerClassName="p-4"
         renderItem={() => null}
         ListHeaderComponent={() => (
-          <View className='flex-row flex-wrap gap-2 justify-center'>
+          <View className="flex-row flex-wrap gap-2 justify-center">
             {menu.map((option, i) => (
               <Link href={option.to} key={option.title + i} asChild>
                 <TouchableOpacity activeOpacity={0.8}>
@@ -49,20 +46,21 @@ const Community = () => {
           </View>
         )}
       />
-
     </DefaultLayout>
-  )
-}
+  );
+};
 
 type CardProps = {
   title: string;
-  icon: any
-}
+  icon: any;
+};
 const Card: React.FC<CardProps> = ({ icon, title }) => {
-  return <View className='bg-blue-400 p-5 items-center  justify-center gap-3 rounded-2xl w-40 h-40'>
-    {icon}
-    <Text className='text-white'>{title}</Text>
-  </View>
-}
+  return (
+    <View className="bg-primario-400 p-5 items-center  justify-center gap-3 rounded-2xl w-40 h-40">
+      {icon}
+      <Text className="text-white">{title}</Text>
+    </View>
+  );
+};
 
-export default Community
+export default Community;
