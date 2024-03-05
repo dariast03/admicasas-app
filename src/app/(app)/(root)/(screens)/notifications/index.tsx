d7d24a3c27af2134fbac2dee1053d8a065e60638
Dante Arias <danteariastarifa@gmail.com>
2024-03-05 01:29:07 -0400

    Refactor notifications screen and update styles

--- src/app/(app)/(root)/(screens)/notifications/index.tsx
+++ src/app/(app)/(root)/(screens)/notifications/index.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { useNotifications } from "@/hooks/useNotifications";
+import useAuth from "@/hooks/useAuth";
+import Loader from "@/components/Loader";
+import NotificationDisplay from "@/components/notifications/notification-display";
 
 const Notifications = () => {
-    return (
-        <View>
-            <Text>Notifications</Text>
-        </View>
-    )
-}
+  const { user } = useAuth();
 
-export default Notifications
+  const { notificationsQuery } = useNotifications({
+    params: {
+      idcondominium: user.account.idcondominium,
+      iduser: user?.account.id || "",
+    },
+    query: ["notificationsQuery"],
+  });
+
+  if (notificationsQuery.isLoading) return <Loader />;
+  if (notificationsQuery.isPending) return <Loader />;
+  if (notificationsQuery.isError || !user?.account?.id)
+    return <Text>HUBO UN ERROR :(</Text>;
+
+  const notifications = notificationsQuery.data;
+
+  return (
+    <View>
+      <FlatList
+        //contentContainerClassName="p-2"
+        data={notifications}
+        ItemSeparatorComponent={() => (
+          <View className=" border-[0.5px] border-primario-200" />
+        )}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <NotificationDisplay notification={item} />}
+      />
+    </View>
+  );
+};
+
+export default Notifications;
