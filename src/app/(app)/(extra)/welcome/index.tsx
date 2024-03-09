7b70714197c709a38ecbf2b5cf24d8bd9d60e5a4
isaacaguilera <aguilera.isaac.az@gmail.com>
2024-03-09 09:56:06 -0400

    Cambios dia

--- src/app/(app)/(extra)/welcome/index.tsx
+++ src/app/(app)/(extra)/welcome/index.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import { useSessionContext } from "../../../../hooks/useSessionContext";
-import { Entypo } from "@expo/vector-icons";
 
 const WelcomeScreen = () => {
   const { handleShowWelcomeScreen } = useSessionContext();
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>SplashScreen Demo! 👋</Text>
-      <Entypo name="rocket" size={30} />
+    <View>
+      <Text>WelcomeScreen</Text>
+
+      <Button
+        title="VER WELCOME"
+        onPress={() => handleShowWelcomeScreen("true")}
+      />
     </View>
   );
 };
